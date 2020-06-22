32c09681d1cea33758cbece7cb04f534a827277a
Lautaro Muller <30838512+lautiimuller@users.noreply.github.com>
2020-06-21 23:59:33 -0300

    Fin tp

--- grupo08.py
+++ grupo08.py
@@ -2,8 +2,8 @@ class Gramatica():
 
     def __init__(self, gramatica):
         """Constructor de la clase."""
-        ListaCadena = gramatica.split("\n")
-        print (ListaCadena)
+        self.ListaCadena = gramatica.split("\n")
+        self.ListaSelect = []
 
         """
             Ejemplo:
@@ -11,7 +11,7 @@ class Gramatica():
         """
         pass
 
-    def isLL1(self, gramatica):
+    def isLL1(self):
         """Verifica si una gramática permite realizar derivaciones utilizando
            la técnica LL1.
         
@@ -20,25 +20,36 @@ class Gramatica():
         resultado : bool
             Indica si la gramática es o no LL1.
         """
-        cadena = gramatica.split("\n")
+        cadena = self.ListaCadena
         esLL1 = True
         listaux = []
         for c in cadena:
             listaux.append(c.split(":"))
-        print ("Lista ordenada", listaux)
+
+
+        for i in listaux:
+            if i[0] == i[1][0]:
+                esLL1 = False #recursion izq
+                return esLL1
+        
+        for i in listaux: #factor comun
+            for x in listaux:
+                if i[1][0] == x[1][0] and listaux.index(i) != listaux.index(x) and i[0] == x[0]: 
+                    esLL1 = False 
+                    return esLL1   
 
         ntconlambda = []
         for item in cadena:
             AnteConc = item.split(":")
             if AnteConc[1] == "lambda":
                 ntconlambda.append(AnteConc[0])
-        print("Antecedentes con Lambda:",ntconlambda)
+        
 
-        first = []
         ListaFirst = []
+        
         firstaux = []
         no_terminal_first = []
-
+        selects = []
         for item in cadena:
             if esLL1:
                 AnteConc = item.split(":")
@@ -46,10 +57,9 @@ class Gramatica():
                 antecedente = str(AnteConc[0])
                 firstaux = []
                 firstaux.extend(AnteConc[0])
-                first.extend(AnteConc[0])
-                c = 0
+                selectaux = []
                 hastaAhi = False
-
+                selectaux.append(item)
                 for caracter in concecuente:
                     if caracter != ' ':
                         if hastaAhi: #Para saber si no tengo que seguir leyendo caracteres
@@ -58,6 +68,8 @@ class Gramatica():
                             hastaAhi = True                             
                             if caracter.islower() and concecuente != "lambda": 
                                 esta = False
+                                selectaux.append(caracter)
+                                selects.append(selectaux)
                                 for i in ListaFirst: #Si ya tiene algun First
                                     if antecedente in i:
                                         esta = True
@@ -68,8 +80,11 @@ class Gramatica():
                                     firstaux.append(caracter)
                                     ListaFirst.append(firstaux)
                                     break
+                                
                             if concecuente == "lambda":
                                 esta = False
+                                selectaux.append(concecuente)
+                                selects.append(selectaux)
                                 for i in ListaFirst: #Si ya tiene algun First
                                     if antecedente in i:
                                         esta = True
@@ -89,6 +104,8 @@ class Gramatica():
                                             hastaAhi = True
                                         if caracter in ireglas[0]: #Encuentro el NT y saco su first
                                             if ireglas[1].islower():
+                                                selectaux.append(ireglas[1])
+                                                selects.append(selectaux)
                                                 esta = False
                                                 for i in ListaFirst: #Si ya tiene algun First
                                                     if antecedente in i:
@@ -96,20 +113,128 @@ class Gramatica():
                                                         esta = True
                                                 if esta:
                                                     if ireglas[1] not in ListaFirst[pos]: #Si ya hay 1
-                                                        ListaFirst[pos].append(ireglas[1]) #Habria F Comun
-                                                        break
+                                                        ListaFirst[pos].append(ireglas[1][0]) #Habria F Comun
+                                                        #break
                                                 else:
                                                     firstaux.append(ireglas[1])
                                                     ListaFirst.append(firstaux)
                                                     break
-                                else:
-                                    # esLL1 = False #recursividad por izq 
-                                    break  #(Lo comento para poder seguir porq la gramatica era recursiva)
-                    
-        print ("")
-        print("Los first son: ", ListaFirst)
+        for aux in selects: #saco selects repetidos
+            if (len(selects)-1) >= selects.index(aux)+1:
+                if aux == selects[selects.index(aux)+1]:
+                    selects.remove(selects[selects.index(aux)+1])
+        
+        ListaFollow = []
+        followaux = []
+        c = 1
+        for item in cadena:             
+            AnteConc = item.split(":")
+            concecuente = str(AnteConc[1])
+            antecedente = str(AnteConc[0]) 
+            followaux = []
+            followaux.extend(AnteConc[0]) 
+            if c == 1:
+                c=2
+                followaux.append("$")
+                ListaFollow.append(followaux)
+            for item2 in listaux: #Lista para encontrar NT en los concecuentes 
+                for i in item2[1]: #recorro el antecedente
+                    if antecedente == i:
+                        cantconc = 0 
+                        cantconc = len(item2[1]) #lo uso para saber si tengo que seguir buscando para adelante
+                        mov = item2[1].index(i) #Si puedo moverme hacia adelante
+                        mov = mov + 2            #muevo
+                        if cantconc < mov:
+                            followaux.append(item2[0]) #No le sigue nada, agrego el antecedente
+                        else:
+                            siguiente = item2[1][mov]                                
+                            esta = False
+                            for x in ListaFollow: #Si ya tiene algun Follow
+                                if antecedente in x[0]:
+                                    pos = ListaFollow.index(x) 
+                                    esta = True
+                            if esta:
+                                if item2[1][mov] not in ListaFollow[pos]: #Si ya hay 1
+                                    ListaFollow[pos].append(item2[1][mov]) 
+                            else:
+                                followaux.append(item2[1][mov])
+                                ListaFollow.append(followaux)
+                            
+                            if siguiente in ntconlambda: #si tiene lambda 
+                                if (mov+2) <= cantconc: #miro si hay algo adelante 
+                                    esta = False
+                                    for x in ListaFollow: #Si ya tiene algun Follow
+                                        if antecedente in x[0]:
+                                            pos = ListaFollow.index(x) 
+                                            esta = True
+                                    if esta:
+                                        if item2[1][mov+2] not in ListaFollow[pos]: #Si ya hay 1
+                                            ListaFollow[pos].append(item2[1][mov+2]) #Habria F Comun
+                                    else:
+                                        followaux.append(item2[1][mov])
+                                        ListaFollow.append(followaux)                                        
+                                else: #sino agrego los follow del concecuente
+                                    followaux.append(item2[0]+":Follows") #F para saber que tengo q sacarle los follows y no los first
+                                    break
 
+        c = 0 #da 10 vueltas para limpiar los NT
+        while c != 10:  
+            c = c + 1                 
+            for lf in ListaFollow:
+                for item in lf:
+                    if item.isupper() and lf.index(item) != 0: #si es un NT y no esta primero
+                        for lf2 in ListaFirst:
+                            #Encuentro el nt y agrego sus follows
+                            if lf2[0] == item:
+                                encontro = False
+                                for x in lf2: 
+                                    if x.islower() and x != "lambda":
+                                        if x not in lf: #si no esta, lo agrega.
+                                            ListaFollow[ListaFollow.index(lf)].append(x)
+                                            encontro = True
+                                ListaFollow[ListaFollow.index(lf)].remove(item)
+                    if "Follows" in item:
+                        itemaux, nosirve = item.split(":")
+                        for lf2 in ListaFollow:
+                            #Encuentro el nt y agrego sus follows
+                            if lf2[0] == itemaux:
+                                encontro = False
+                                for x in lf2: 
+                                    if x.islower():
+                                        if x not in lf: #si no esta, lo agrega.
+                                            ListaFollow[ListaFollow.index(lf)].append(x)
+                                            encontro = True
+                                ListaFollow[ListaFollow.index(lf)].remove(item)
+                    for y in range(1,len(lf)):
+                        if lf[y].isupper() and lf[0] == lf[y]:
+                           lf.pop(y)#Si quedo un NT igual al antecedente.
+        #Donde hay lambda, meto sus follows
+        for s in selects:
+            if s[1] == "lambda":
+                selects[selects.index(s)].remove(s[1]) 
+                for lf in ListaFollow: #Busco los follows
+                    ante = s[0].split(":")
+                    if lf[0] == ante[0]:
+                        for i in range (1,len(lf)): #sin contar el primero que es un NT
+                            selects[selects.index(s)].append(lf[i]) #Agrego los follows de el
 
+        #Verifico si hay disyuntos
+        for sel1 in selects:
+            antsel = sel1[0].split(":")
+            for sel2 in selects:
+                antsel2 = sel2[0].split(":")
+                if sel1 != sel2 and antsel[0] == antsel2[0]:
+                    for aux1 in antsel[1]:
+                        for aux2 in antsel2[1]:
+                            if aux1 == aux2 and aux1 != " ":
+                                esLL1 = False #disyuntos  
+        
+        
+        for i, x in enumerate(selects): 
+            caracteres = x[1]    
+            self.ListaSelect.append(caracteres)
+            
+        return esLL1             
     def parse(self, cadena):
         """Retorna la derivación para una cadena dada utilizando las
            producciones de la gramática y los conjuntos de Fi, Fo y Se
@@ -131,7 +256,11 @@ class Gramatica():
         """
         pass
 
-# Original lista = Gramatica("A:b A\nA:a\nA:A B c\nA:lambda\nB:b").isLL1("A:b A\nA:a\nA:A B c\nA:lambda\nB:b")
+#lista = Gramatica("A:b A\nB:a\nA:h\nA:B c\nA:lambda\nB:b").isLL1("A:b A\nB:a\nA:h\nA:B c\nA:lambda\nB:b")
 
 #Esta es de prueba
-lista = Gramatica("A:b A\nA:B\nA:B C c\nB:lambda\nB:b\nC:h\nB:c").isLL1("A:b A\nA:B\nA:B C c\nB:lambda\nB:b\nC:h\nB:c")
+#lista = Gramatica("A:b A\nA:B\nA:B C c\nB:lambda\nB:b\nC:h\nB:c").isLL1("A:b A\nA:B\nA:B C c\nB:lambda\nB:b\nC:h\nB:c")
+
+#Prueba  follows
+lista = Gramatica("B:A a\nA:B a\nC:A B E\nE:g\nE:lambda\nB:c\nA:E a\nB:C h\nC:i e").isLL1()
+
